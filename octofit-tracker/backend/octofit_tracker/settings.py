9f4288e530bb429ac44f4cf68238be0248b7a917
Wolves2013 <logan.ivester@northgatech.edu>
2025-04-09 18:01:08 +0000

    modified:   octofit-tracker/backend/octofit_tracker/settings.py

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -77,6 +77,7 @@ WSGI_APPLICATION = "octofit_tracker.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# Ensure the database engine is explicitly set to djongo
 DATABASES = {
     'default': {
         'ENGINE': 'djongo',
